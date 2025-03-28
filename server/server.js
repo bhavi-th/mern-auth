6df812396f86b61ccb5a6f0149f9a1ccc8f23220
Bhavith <bhaviths20@gmail.com>
2025-03-28 21:48:24 +0530

    Connected to database

--- server/server.js
+++ server/server.js
@@ -2,14 +2,19 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js'
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
-app.get('/', (req, res) => res.send("API working"))
+//API endpoints
+app.get('/', (req, res) => res.send("API working"));
+app.use('/api/auth', authRouter);
 
 app.listen(port, () => console.log(`Server started in port : ${port}`));
